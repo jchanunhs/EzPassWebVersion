2e1ec15ae1c92f5cf8296efce9223a8a980dfdbb
jchanunhs <53135155+jchanunhs@users.noreply.github.com>
2020-05-26 13:17:00 -0400

    If removing tag failed, check if tag input is valid.

--- EzPassSpring/src/java/control/RemoveTagControl.java
+++ EzPassSpring/src/java/control/RemoveTagControl.java
@@ -16,14 +16,17 @@ public class RemoveTagControl {
     @RequestMapping(value = "/RemoveTagControl", method = RequestMethod.POST)
     public ModelAndView RemoveTag(HttpServletRequest request, HttpServletResponse response, ModelAndView mv, RedirectAttributes redirectAttributes) {
         HttpSession session = request.getSession();
-        String CID = (String)session.getAttribute("CID");
+        String CID = (String) session.getAttribute("CID");
         String TC = request.getParameter("TagCode");
         EzTag ez = new EzTag(TC, CID);
         mv.setViewName("redirect:/RemoveTag");
         if (ez.removeTag()) {
             redirectAttributes.addFlashAttribute("message", "EzTag was removed successfully!");
+        } //if fails to add tag, we try to figure out why it happened
+        else if (!ez.checkTag()) {
+            redirectAttributes.addFlashAttribute("message", "Error: Tag code is invalid");
         } else {
-            redirectAttributes.addFlashAttribute("message", "Error: Either EzTag is invalid or it's currently linked to a vehicle and has been used to pay tolls in the past. If you believe this is a mistake please contact help desk.");
+            redirectAttributes.addFlashAttribute("message", "Error: Either EzTag is currently associated with a vehicle or has been used to pay tolls in the past. If you believe this is a mistake please contact help desk.");
         }
         return mv;
     }
